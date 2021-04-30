2d5498285a0367917b31541452714755f6f59c37
ggggg <ggggg@DESKTOP-NQI421E>
2021-05-01 01:25:21 +0700

    Немного подправил

---
+++ ConsoleApp12/Program1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace HelloApp
+{
+    class Program1
+    {
+        delegate List<T> Operation<T>(BinaryTree<T> tree);
+        static List<T> Func<T>(BinaryTree<T> Tree)
+        {
+            Operation<T> operation = (BinaryTree<T> tree) =>
+            {
+                List<T> values = new List<T>();
+                foreach (T value in tree)
+                    values.Add(value);
+                return values;
+            };
+            return operation(Tree);
+        }
+    }
+}
